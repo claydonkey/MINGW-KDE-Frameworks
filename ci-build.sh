ccbeadc050d1ca359c85971d9de6ed61909799b2
Anthony Campbell <anthony@claydonkey.com>
2017-08-22 14:01:35 +0100

    touch + prefix

--- ci-build.sh
+++ ci-build.sh
@@ -22,11 +22,12 @@ test -z "${packages}" && success 'No changes in package recipes'
 define_build_order || failure 'Could not determine build order'
 
 # Build
-export MINGW_PACKAGE_PREFIX=
+export MINGW_PREFIX=mingw-w64
+export MINGW_PACKAGE_PREFIX=mingw-w64-x86_64
 message 'Building packages' "${packages[@]}"
 execute 'Updating system' update_system
 execute 'Approving recipe quality' check_recipe_quality
-export MINGW_PACKAGE_PREFIX=mingw-w64
+
 for package in "${packages[@]}"; do
     execute 'Building binary' makepkg  --noconfirm  --skippgpcheck --nocheck --syncdeps --rmdeps --cleanbuild
     execute 'Building source' makepkg --noconfirm --noprogressbar --skippgpcheck --allsource --config '/etc/makepkg_mingw64.conf'
